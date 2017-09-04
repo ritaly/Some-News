e1cdd1bb04a5f92405a0163526c1eb794e54a415
ritaly <malgorzata.lyczywek@gmail.com>
2017-09-04 13:54:57 +0200

    Routing in AngularJS with ui-router

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+    .config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: '/home.html',
+          controller: 'MainCtrl'
+        });
+
+      $urlRouterProvider.otherwise('home');
+    }])
+    .factory ('posts', [function(){
+            var o = {
+            posts: [
+              {title: 'post 1', upvotes: 5},
+              {title: 'post 2', upvotes: 2},
+              {title: 'post 3', upvotes: 15},
+              {title: 'post 4', upvotes: 9},
+              {title: 'post 5', upvotes: 4}
+              ]
+             };
+            return o;
+        }])
+
     .controller('MainCtrl', [
-    '$scope',
-    function($scope){
-        $scope.test = 'Hello world!';
-        $scope.posts = [
-          {title: 'post 1', upvotes: 5},
-          {title: 'post 2', upvotes: 2},
-          {title: 'post 3', upvotes: 15},
-          {title: 'post 4', upvotes: 9},
-          {title: 'post 5', upvotes: 4}
-        ];
+    '$scope','posts',
+    function($scope, posts){
+        $scope.posts = posts.posts;
+        $scope.addPost = function(){
+          if(!$scope.title || $scope.title === '') { return; }
+          $scope.posts.push({
+            title: $scope.title,
+            link: $scope.link,
+            upvotes: 0
+          });
+          $scope.title = '';
+          $scope.link = '';
+        };
+        $scope.incrementUpvotes = function(post) {
+          post.upvotes += 1;
+        };
 }]);
