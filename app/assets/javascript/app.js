d58e3002275be404d31cb704bdaf7a4658196145
ritaly <malgorzata.lyczywek@gmail.com>
2017-09-05 11:54:44 +0200

    Integrating AngularJS with the Rails Asset Pipeline

--- app/assets/javascript/app.js
+++ app/assets/javascript/app.js
@@ -7,30 +7,19 @@ angular.module('flapperNews', ['ui.router', 'templates'])
       $stateProvider
         .state('home', {
           url: '/home',
-          templateUrl: '/home.html',
+          templateUrl: 'home/_home.html',
           controller: 'MainCtrl'
         })
         .state('posts', {
           url: '/posts/{id}',
-          templateUrl: '/posts.html',
+          templateUrl: 'posts/_posts.html',
           controller: 'PostsCtrl'
         });
       $urlRouterProvider.otherwise('home');
     }])
 
 
-  .factory ('posts', [function(){
-            var o = {
-            posts: [
-              {title: 'post 1', upvotes: 5, comments: []},
-              {title: 'post 2', upvotes: 2, comments: []},
-              {title: 'post 3', upvotes: 15, comments: [{author: 'Jon', body: 'Winter is comming!', upvotes: 3}]},
-              {title: 'post 4', upvotes: 9, comments: []},
-              {title: 'post 5', upvotes: 4, comments: []}
-              ]
-             };
-            return o;
-        }])
+ 
 
     .controller('PostsCtrl', [
         '$scope',
@@ -49,25 +38,3 @@ angular.module('flapperNews', ['ui.router', 'templates'])
           };
         }])
 
-    .controller('MainCtrl', [
-    '$scope','posts',
-    function($scope, posts){
-        $scope.posts = posts.posts;
-        $scope.addPost = function(){
-          if(!$scope.title || $scope.title === '') { return; }
-          $scope.posts.push({
-            title: $scope.title,
-            link: $scope.link,
-            upvotes: 0,
-            comments: [
-              {author: 'Joe', body: 'Cool post!', upvotes: 0},
-              {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-            ]
-          });
-          $scope.title = '';
-          $scope.link = '';
-        };
-        $scope.incrementUpvotes = function(post) {
-          post.upvotes += 1;
-        };
-    }]);
